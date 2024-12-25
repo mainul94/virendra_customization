fd4f16db5c21b7d4e71e90b6eb6fe827b427f576
samsul580 <samsulshekh2019@gmail.com>
2024-12-25 20:19:54 +0600

    feat: add Variant doctype with initial structure and test cases

---
+++ virendra_customization/virendra_customization/doctype/variant/test_variant.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Cloudy Camp Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestVariant(FrappeTestCase):
+	pass
