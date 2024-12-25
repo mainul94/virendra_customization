ae37c22a32d0b5ff7ab6902673646ab35da796e9
Mainul <mainulkhan94@gmail.com>
2024-12-25 09:33:45 +0600

    freat: Added API for receiving Webhook request from wasender and create Leade.

---
+++ virendra_customization/api.py
@@ -0,0 +1,20 @@
+import frappe
+
+
+@frappe.whitelist()
+def wasender_message(**kwargs):
+    contact = kwargs.get("contact", {})
+    _map_dict = {
+        "mobile_no": contact.get("phone_number"),
+        "whatsapp_no": contact.get("phone_number"),
+        "email_id": contact.get("email"),
+        "first_name": contact.get("first_name"),
+        "last_name": contact.get("last_name"),
+        "country": contact.get("country"),
+    }
+    if not frappe.db.exists('Lead', _map_dict):
+        lead = frappe.new_doc("Lead")
+        lead.update(_map_dict)
+        lead.insert(ignore_permissions=True)
+    return 'Success'
+
