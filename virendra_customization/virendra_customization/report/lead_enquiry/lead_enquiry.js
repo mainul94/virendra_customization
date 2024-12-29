bf457c292947940a2f454c654430342ea2e86231
samsul580 <samsulshekh2019@gmail.com>
2024-12-29 12:35:41 +0600

    refactor: update lead enquiry report fields and labels for clarity

--- virendra_customization/virendra_customization/report/lead_enquiry/lead_enquiry.js
+++ virendra_customization/virendra_customization/report/lead_enquiry/lead_enquiry.js
@@ -14,12 +14,6 @@ frappe.query_reports["Lead Enquiry"] = {
 			"fieldtype": "Data"
 		},
 		{
-			"fieldname": "status",
-			"label": __("Status"),
-			"fieldtype": "Select",
-			"options": [" ", "Test Drive Completed", "Retail", "Booking", "Lost"]
-		},
-		{
 			"fieldname":"from_date",
 			"label": __("From Date"),
 			"fieldtype": "Date"
@@ -37,7 +31,7 @@ frappe.query_reports["Lead Enquiry"] = {
 		},
 		{
 			"fieldname":"model",
-			"label": __("Model"),
+			"label": __("Vehicle"),
 			"fieldtype": "Link",
 			"options": "Model"
 		},
@@ -54,16 +48,27 @@ frappe.query_reports["Lead Enquiry"] = {
 			"options": "City"
 		},
 		{
-			"fieldname":"lead_owner",
-			"label": __("Lead Owner"),
-			"fieldtype": "Link",
-			"options": "User"
+			"fieldname":"pincode",
+			"label": __("Pincode"),
+			"fieldtype": "Data"
+		},
+		{
+			"fieldname":"status",
+			"label": __("Lead Status"),
+			"fieldtype": "Select",
+			"options": [" ", "Test Drive Completed", "Retail", "Booking", "Lost"]
 		},
 		{
 			"fieldname":"lead_type",
-			"label": __("Lead Type"),
+			"label": __("Lead Progress"),
 			"fieldtype": "Select",
 			"options": [" ", "Client", "Channel Partner", "Consultant"]
+		},
+		{
+			"fieldname":"lead_owner",
+			"label": __("Assigned To"),
+			"fieldtype": "Link",
+			"options": "User"
 		}
 	]
 };
